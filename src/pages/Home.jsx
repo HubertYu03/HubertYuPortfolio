1e79cd3605664b2c85329f47b046be2ad3054cdc
hubbbbbbbbb <hubyu9234@gmail.com>
2024-03-02 18:04:09 -0800

    Home header animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,5 @@
 import { useEffect } from "react";
-import {
-  AnimatePresence,
-  easeIn,
-  easeInOut,
-  easeOut,
-  motion,
-} from "framer-motion";
+import { AnimatePresence, easeOut, motion } from "framer-motion";
 
 import "../styles/Home.css";
 
@@ -15,12 +9,45 @@ const Home = ({ setCurrentPage }) => {
     setCurrentPage("home-link");
   }, []);
 
+  const height = window.innerHeight;
+
+  const title = "Hubert Yu";
+  const titleChars = title.split("");
+
   const bannerVariant = {
     visible: {
       y: 0,
-      transition: { duration: 2, ease: [0.01, 0.1, 0.2, 0.999] },
+      transition: { duration: 1.5, ease: [0.01, 0.1, 0.2, 0.999] },
     },
-    hidden: { y: 1000 },
+    hidden: { y: height },
+  };
+
+  const titleContainerVariant = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.1, delayChildren: 0.01 * i },
+    }),
+  };
+
+  const characterVariant = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: easeOut,
+        duration: 0.3,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 10,
+    },
+  };
+
+  const subTitleVariant = {
+    visible: { opacity: 1, y: 0, transition: { delay: 1.8 } },
+    hidden: { opacity: 0, y: 20 },
   };
 
   return (
@@ -36,6 +63,41 @@ const Home = ({ setCurrentPage }) => {
       </motion.div>
 
       {/* Page Header */}
+      <div className="page-container">
+        <div className="header-container">
+          <div className="header">
+            <motion.div
+              className="title"
+              variants={titleContainerVariant}
+              initial="hidden"
+              animate="visible"
+            >
+              {titleChars.map((char, index) => (
+                <motion.span
+                  variants={characterVariant}
+                  key={index}
+                  style={{ marginLeft: "3px" }}
+                >
+                  {char}
+                </motion.span>
+              ))}
+            </motion.div>
+            <motion.div
+              className="sub-title"
+              initial="hidden"
+              animate="visible"
+              variants={subTitleVariant}
+            >
+              Aspiring Web Developer.
+            </motion.div>
+          </div>
+        </div>
+        <div className="bio">
+          I am a computer science and engineering student at the University of
+          California Merced. I am passionate about creating engaging user
+          interfaces and experiences.
+        </div>
+      </div>
     </AnimatePresence>
   );
 };
